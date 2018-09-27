bb0654f95aba08c9cd6afc3191ddd52aefc28ec6
higa <higa@key-p.jp>
2018-09-27 17:40:02 +0900

    update

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
 
+  console.log('sw:fetch');
+
+  event.respondWith(
+    caches.match(event.request.url).then(response => {
+      console.log(response);
+      return fetch(event.request.clone());
+    })
+  );
 });
