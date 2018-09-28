0cc8662856ee456688ccf9356d215d62c2ec696d
higa <higa@key-p.jp>
2018-09-28 17:51:07 +0900

    update

--- init.js
+++ init.js
@@ -1,15 +1,17 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/pages/sw.js').then((reg) => {
-    console.log(
-      reg.installing ? 'init:installing' : '',
-      reg.waiting ? 'init:waiting' : '',
-      reg.active ? 'init:active' : '',
-    );
-  });
+$(function () {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/pages/sw.js').then((reg) => {
+      console.log(
+        reg.installing ? 'init:installing' : '',
+        reg.waiting ? 'init:waiting' : '',
+        reg.active ? 'init:active' : '',
+      );
+    });
 
-  navigator.serviceWorker.addEventListener('message', onMessage);
-}
+    navigator.serviceWorker.addEventListener('message', onMessage);
+  }
 
-function onMessage(event) {
-  $('#messages').append(`<p>${event.data.message}</p>`);
-}
+  function onMessage(event) {
+    $('#messages').append(`<p>${event.data.message}</p>`);
+  }
+});
